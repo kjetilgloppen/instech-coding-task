513e7d6f3f630f0461948f13473853b6a5c8dcab
Kjetil Gloppen <kg@danelec.com>
2024-06-13 01:55:52 +0200

    Task 4: Adding more tests
    Adding tests for all the API endpoints in the ClaimsController.
    Part of this was to move the code for setting up the application
    into the constructor so that it can be used by all tests.
    
    Notice that the Delete_Claim() test fails. We would expect trying to
    delete an Claim with non-existing ID would fail, but current code returns
    success. Will fix this in the next commit.

--- Claims.Tests/ClaimsControllerTests.cs
+++ Claims.Tests/ClaimsControllerTests.cs
@@ -1,24 +1,55 @@
-﻿using Microsoft.AspNetCore.Mvc.Testing;
+﻿using System.Net;
+using System.Net.Http.Json;
+using Microsoft.AspNetCore.Mvc.Testing;
 using Xunit;
 
 namespace Claims.Tests;
 
 public class ClaimsControllerTests
 {
+    private readonly HttpClient _client;
+    public ClaimsControllerTests()
+    {
+        var application = new WebApplicationFactory<Program>()
+            .WithWebHostBuilder(_ => { });
+
+        _client = application.CreateClient();
+    }
+
     [Fact]
     public async Task Get_Claims()
     {
-        var application = new WebApplicationFactory<Program>()
-            .WithWebHostBuilder(_ =>
-                {});
-
-        var client = application.CreateClient();
-
-        var response = await client.GetAsync("/Claims");
+        var response = await _client.GetAsync("/Claims");
 
         response.EnsureSuccessStatusCode();
 
         //TODO: Apart from ensuring 200 OK being returned, what else can be asserted?
     }
 
+    [Fact]
+    public async Task Get_Claim()
+    {
+        var response = await _client.GetAsync("/Claims/id");
+        response.EnsureSuccessStatusCode();
+    }
+
+    [Fact]
+    public async Task Add_Claim()
+    {
+        var badClaim = new Claim
+        {
+            Id = "test",
+            CoverId = "bad-id",
+        };
+        var content = JsonContent.Create(badClaim);
+        var response = await _client.PostAsync("/Claims", content);
+        Assert.Equal(HttpStatusCode.BadRequest, response.StatusCode);
+    }
+    
+    [Fact]
+    public async Task Delete_Claim()
+    {
+        var response = await _client.DeleteAsync("/Claims/id");
+        Assert.False(response.IsSuccessStatusCode);
+    }
 }
