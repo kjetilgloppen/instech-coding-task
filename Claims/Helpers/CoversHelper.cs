3f52780d1bf8196258b00a4fc0e4943b0a6633d3
Kjetil Gloppen <kg@danelec.com>
2024-06-13 10:41:00 +0200

    Premium calculation cleanup
    - use switch statements to set multiplier for different vessel types
    - format if-statement to easier get an overview of what's being done
    
    Doing this cleanup makes it easier to see that there are some missing
    else-statements so totalPremium is incremented multiple times in the
    for-loop.

--- Claims/Helpers/CoversHelper.cs
+++ Claims/Helpers/CoversHelper.cs
@@ -4,21 +4,13 @@ public static class CoversHelper
 {
     public static decimal ComputePremium(DateTime startDate, DateTime endDate, CoverType coverType)
     {
-        var multiplier = 1.3m;
-        if (coverType == CoverType.Yacht)
+        var multiplier = coverType switch
         {
-            multiplier = 1.1m;
-        }
-
-        if (coverType == CoverType.PassengerShip)
-        {
-            multiplier = 1.2m;
-        }
-
-        if (coverType == CoverType.Tanker)
-        {
-            multiplier = 1.5m;
-        }
+            CoverType.Yacht => 1.1m,
+            CoverType.PassengerShip => 1.2m,
+            CoverType.Tanker => 1.5m,
+            _ => 1.3m
+        };
 
         var premiumPerDay = 1250 * multiplier;
         var insuranceLength = (endDate - startDate).TotalDays;
@@ -26,11 +18,28 @@ public static class CoversHelper
 
         for (var i = 0; i < insuranceLength; i++)
         {
-            if (i < 30) totalPremium += premiumPerDay;
-            if (i < 180 && coverType == CoverType.Yacht) totalPremium += premiumPerDay - premiumPerDay * 0.05m;
-            else if (i < 180) totalPremium += premiumPerDay - premiumPerDay * 0.02m;
-            if (i < 365 && coverType != CoverType.Yacht) totalPremium += premiumPerDay - premiumPerDay * 0.03m;
-            else if (i < 365) totalPremium += premiumPerDay - premiumPerDay * 0.08m;
+            if (i < 30)
+            {
+                totalPremium += premiumPerDay;
+            }
+
+            if (i < 180 && coverType == CoverType.Yacht)
+            {
+                totalPremium += premiumPerDay - premiumPerDay * 0.05m;
+            }
+            else if (i < 180)
+            {
+                totalPremium += premiumPerDay - premiumPerDay * 0.02m;
+            }
+
+            if (i < 365 && coverType != CoverType.Yacht)
+            {
+                totalPremium += premiumPerDay - premiumPerDay * 0.03m;
+            }
+            else if (i < 365)
+            {
+                totalPremium += premiumPerDay - premiumPerDay * 0.08m;
+            }
         }
 
         return totalPremium;
